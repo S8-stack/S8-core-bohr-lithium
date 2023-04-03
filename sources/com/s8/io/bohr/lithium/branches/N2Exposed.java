163889d59fbf56ef5bf1ceaae428a34a2d62a7e1
Pierre Convert <pierreconvert@macbook-pro-6.lan>
2023-04-03 18:42:14 +0200

    independent BOHR/implementation

---
+++ sources/com/s8/io/bohr/lithium/branches/N2Exposed.java
@@ -0,0 +1,18 @@
+package com.s8.io.bohr.lithium.branches;
+
+/**
+ * 
+ * 
+ * @author Pierre Convert
+ * Copyright (C) 2022, Pierre Convert. All rights reserved.
+ *
+ */
+public class N2Exposed {
+	
+	public String index;
+
+	public N2Exposed(String index) {
+		super();
+		this.index = index;
+	}	
+}
